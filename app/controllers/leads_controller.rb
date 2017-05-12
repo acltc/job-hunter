39a816c4a7284e5c2fc6e0f87150a1c66b6d026a
rymcmahon <toedragdangle9@gmail.com>
2017-05-12 16:28:56 -0500

    fixed lead user id nil error and added lead index styling

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,6 +1,5 @@
 class LeadsController < ApplicationController
   before_action :authenticate_user!
-  # before_action :lead_owner, only: [:edit, :update]
 
   def index
     @leads = current_user.leads
@@ -24,13 +23,11 @@ class LeadsController < ApplicationController
   end
 
   def edit
-    @lead = Lead.find(params[:id])
-    @lead.user_id = @lead.user_id
+    authorize_lead_owner
   end
 
   def update
-    @lead = Lead.find(params[:id])
-
+    authorize_lead_owner
     if @lead.update
       redirect_to @lead
     else
@@ -44,11 +41,12 @@ class LeadsController < ApplicationController
       parmas.require(:lead).permit(:first_name, :last_name, :email, :phone, :company, :linkedin_url, :email_sent, :meeting_set, :interview_set, :job_offer)
     end
 
-    # def lead_owner
-    #   unless @lead.user_id == current_user.id || current_user.admin
-    #     flash[:notice] = 'Access Denied'
-    #     redirect_to leads_path
-    #   end
-    # end
+    def authorize_lead_owner
+      @lead = Lead.find(params[:id])
+      unless @lead.user_id == current_user.id || current_user.admin
+        flash[:notice] = 'Access Denied'
+        redirect_to leads_path
+      end
+    end
 
 end
