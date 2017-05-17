13086c64ad226f4c42ecbbbb151ed775a588c955
rymcmahon <toedragdangle9@gmail.com>
2017-05-17 11:25:13 -0500

    added devise users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  require 'csv'
+
   has_many :leads
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def self.import(file)
+    CSV.foreach(file.path, header: true) do |row|
+      User.create! row.to_hash
+    end
+  end
 end
