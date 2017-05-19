f53b2cee850dff0da0bbf531c62f9d6e238460cb
rymcmahon <toedragdangle9@gmail.com>
2017-05-19 17:20:24 -0500

    added interviewer name and offer status to leads and added those fields to new form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511193630) do
+ActiveRecord::Schema.define(version: 20170519205351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170511193630) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "interviewer_name"
+    t.string "offer_status"
     t.index ["user_id"], name: "index_leads_on_user_id"
   end
 
