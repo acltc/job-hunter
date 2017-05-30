486418e1f2c370eea79e50848a29eb7b9b0cc56f
rymcmahon <toedragdangle9@gmail.com>
2017-05-30 13:55:16 -0500

    added find by user and create lead to email processor class

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,6 +4,8 @@ class EmailProcessor
   end
 
   def process
-    Lead.create!(email: @email.to )
+    # Lead.create!(email: @email.to)
+    @user = User.find_by(email: @email.from)
+    @user.leads.create!(email: @email.to)
   end
 end
