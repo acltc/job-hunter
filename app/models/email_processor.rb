400da63dd1e182577829868acba006a3ec71a356
rymcmahon <toedragdangle9@gmail.com>
2017-05-30 14:16:05 -0500

    added hash value to email find by

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,7 +5,7 @@ class EmailProcessor
 
   def process
     # Lead.create!(email: @email.to)
-    @user = User.find_by!(email: @email.from)
+    @user = User.find_by!(email: @email.from[:email])
     @user.leads.create!(email: @email.to)
   end
 end
