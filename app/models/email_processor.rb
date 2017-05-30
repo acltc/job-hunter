b32719703d02ffd3cf58fb18db830b15c604140e
rymcmahon <toedragdangle9@gmail.com>
2017-05-30 14:35:26 -0500

    changed find by

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,8 +4,9 @@ class EmailProcessor
   end
 
   def process
-    # Lead.create!(email: @email.to)
-    @user = User.find_by!(email: @email.from[:email])
-    @user.leads.create!(email: @email.to)
+    user = User.find_by_email(@email.from[:email])
+    user.leads.create!(
+      email: @email.to
+    )
   end
 end
