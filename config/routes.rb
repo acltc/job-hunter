75a51d8ac55009326116acf01dfe1381cfa231b0
Jay Wengrow <jaywngrw@gmail.com>
2017-07-06 12:58:52 -0500

    update first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'leads#index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users do
     collection { post :import }
   end
